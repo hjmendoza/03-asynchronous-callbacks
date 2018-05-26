6f4b02a5c0aee3428162bcf87a88f503eed1a6c8
Haley Mendoza <mendozahaley@gmail.com>
2018-05-25 18:56:08 -0700

    passing for any non-existent file path passed.

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -1,23 +1,20 @@
 'use strict';
 
-const reader = require('../lib/reader.js');
+const read = require('../lib/reader.js');
 
 describe('File Reader Module', () => {
 
   it('should callback with error if a file does not exist', (done) => {
-    done();
+    read(['nonexisting.txt'],(err) => {
+      expect(err).not.toBeNull();
+      done();
+    });
   });
 
-  xit('should call with error if for any file that does not exist', (done) => {
-
+  it('should call with error if for any file that does not exist', (done) => {
     read([__dirname + '/data/1.txt', 'nonexisting.txt'], (err) => {
       expect(err).not.toBeNull();
-    })
-    // let file = `${__dirname}/../../data/file1.txt`;
-    // reader(file, (err,data) => {
-    //   expect(err).toBeUndefined();
-    //   // We don't need to care what the text is, only that we got back a string
-    //   // That's the interface of our reader module: Give a file+cb, get back stringified  contents
-    //   expect(typeof data).toBe('string');
+      done();
+    });
   });
 });
