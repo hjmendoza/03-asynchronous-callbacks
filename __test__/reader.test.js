e2c30275b7c69402cebd0a8ba1871ac0084be5f5
Haley Mendoza <mendozahaley@gmail.com>
2018-05-25 21:07:51 -0700

    all tests passing.

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -3,7 +3,6 @@
 const read = require('../lib/reader.js');
 
 describe('File Reader Module', () => {
-
   it('should callback with error if a file does not exist', (done) => {
     read(['nonexisting.txt'],(err) => {
       expect(err).not.toBeNull();
@@ -11,7 +10,7 @@ describe('File Reader Module', () => {
     });
   });
 
-  it('should call with error if for any file that does not exist', (done) => {
+  it('should callback with error if any file does not exist', (done) => {
     read([__dirname + '/data/1.txt', 'nonexisting.txt'], (err) => {
       expect(err).not.toBeNull();
       done();
@@ -21,7 +20,21 @@ describe('File Reader Module', () => {
   it('should return data from a file', (done) => {
     read([__dirname + '/../data/1.txt'], (err, data) => {
       expect(err).toBeUndefined();
-      expect(data.toString()).toBe('First file.');
+      expect(data.toString()).toBe('first file.');
+      done();
+    });
+  });
+
+  it('should return data for all files', (done) => {
+    let files = [];
+    for (let items of ['1', '2', '3']) {
+      files.push(__dirname + '/../data/' + items + '.txt');
+    }
+    read(files, (err, data) => {
+      expect(err).toBeUndefined();
+      expect(data[0]).toBe('first file.');
+      expect((data[1]).startsWith('second.')).toBe(true);
+      expect((data[2]).startsWith('Third text.')).toBe(true);
       done();
     });
   });
